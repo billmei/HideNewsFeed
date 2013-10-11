a3c6be4f65458d3ac21f8ce3d1f81b9c05f28ca8
Bill Mei <contact@kortaggio.com>
2013-10-11 22:45:02 +0200

    It works! Version 0.1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function(){
+
+$(document).ready(function(){
+    if ($('#pagelet_home_stream').length > 0) {
+        hideNewsFeed();
+    }
+//Facebook pulls data in via scripts so we need to re-check if the user has navigated to a new page every second.
+window.setInterval(function(){
+    if ($('#pagelet_home_stream').length > 0) {
+        hideNewsFeed();
+    }
+},1000);
+});
+
+function hideNewsFeed() {
+    $('#pagelet_home_stream').hide();
+}
+
+}());
+
