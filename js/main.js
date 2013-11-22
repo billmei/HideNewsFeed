2d0afeb18bc015394618487a0099995f350cfe04
Bill Mei <billmei@users.noreply.github.com>
2013-11-22 18:11:43 +0100

    version 0.3
    Updated because Facebook changed the newsfeed ID

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 (function(){
 
 $(document).ready(function(){
-    if ($('#pagelet_home_stream').length > 0) {
+    if ($('#topnews_main_stream_408239535924329').length > 0) {
         hideNewsFeed();
     }
 //Facebook pulls data in via scripts so we need to re-check if the user has navigated to a new page every second.
 window.setInterval(function(){
-    if ($('#pagelet_home_stream').length > 0) {
+    if ($('#topnews_main_stream_408239535924329').length > 0) {
         hideNewsFeed();
     }
 },20000);
 });
 
 function hideNewsFeed() {
-    $('#pagelet_home_stream').remove();
+    $('#topnews_main_stream_408239535924329').remove();
 }
 
 }());
