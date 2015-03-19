f7ae5896f5c6118d5a18702cff6370fb84d6a3c9
Bill Mei <billmei@users.noreply.github.com>
2015-03-18 23:13:17 -0400

    Remove jQuery

--- Chrome Extension/js/main.js
+++ Chrome Extension/js/main.js
@@ -1,19 +1,14 @@
 (function(){
 
-$(document).ready(function(){
-    if ($('#topnews_main_stream_408239535924329').length > 0) {
-        hideNewsFeed();
-    }
-//Facebook pulls data in via scripts so we need to re-check if the user has navigated to a new page every second.
-window.setInterval(function(){
-    if ($('#topnews_main_stream_408239535924329').length > 0) {
-        hideNewsFeed();
-    }
-},20000);
+document.addEventListener("DOMContentLoaded", function(event) {
+	var newsfeed = document.getElementById('topnews_main_stream_408239535924329');
+  if (newsfeed) {
+    hideElement(newsfeed);
+  }
 });
 
-function hideNewsFeed() {
-    $('#topnews_main_stream_408239535924329').remove();
+function hideElement(el) {
+	el.parentNode.removeChild(el);
 }
 
 }());
