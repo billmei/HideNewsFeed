3a46a842f393b25a60d93591129242d889990099
Bill Mei <billmei@users.noreply.github.com>
2015-08-02 19:02:19 -0700

    Hide trending topics

--- Chrome Extension/js/main.js
+++ Chrome Extension/js/main.js
@@ -1,15 +1,16 @@
 (function(){
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	var newsfeed = document.getElementById('topnews_main_stream_408239535924329');
-  if (newsfeed) {
-    hideElement(newsfeed);
-  }
+	var idsToHide = ['pagelet_trending_tags_and_topics', 'topnews_main_stream_408239535924329'];
+	for (int i = 0; i < idsToHide.length; i++) {
+		var item = document.getElementById('topnews_main_stream_408239535924329');
+		if (item) {
+			hideElement(item);
+		}
+	}
 });
 
 function hideElement(el) {
 	el.parentNode.removeChild(el);
 }
-
 }());
-
